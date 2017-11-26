4091a067983685d4bb85c64645b0c81c7e15d35a
a45s67 <32732820+a45s67@users.noreply.github.com>
2017-11-27 03:15:28 +0800

    Add files via upload

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,15 +1,20 @@
 #==============
 import numpy as np
-
+import pickle
 with open('training_label.txt','r',encoding='utf-8') as f:
     trainlist=f.readlines()
 list_x=[]
 list_y=[]
+seqlen=0
 for i in range(len(trainlist)):
         
-    train=trainlist[i].strip('\n').split(' ')
+    train=trainlist[i].strip('\n').split('+++$+++')
     y=train[0]
-    x=train[2:len(train)]
+    x=train[1]
+    length=len(x.split(' '))
+    if length>seqlen:
+        seqlen=length
+        
     list_x.append(x)
     list_y.append(y)
     
@@ -25,20 +30,102 @@ def listtostr(list):
         x=np.append(x,np.array(list_x[i]))
  #       print (x)
     return x
-str_x=listtostr(list_x)
+#str_x=listtostr(list_x)
+str_x=list_x
 MAX_NB_WORDS=20000
-
+from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import one_hot
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
+from keras.utils import to_categorical
 
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(str_x)
 sequences = tokenizer.texts_to_sequences(str_x)
- #   print(i)
-word_index = tokenizer.word_index
+print(sequences[0])
+word_index=tokenizer.word_index
+word_size = len(word_index)+1
 print('Found %s unique tokens.' % len(word_index))
-print('Found %s unique words.' % len(str_x))
-print(x)
-data = pad_sequences(sequences, maxlen=5000)
 
+print('maxseqlen=',seqlen)
+data = pad_sequences(sequences, maxlen=seqlen)
+labels = to_categorical(np.asarray(list_y))
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+nb_validation_samples = int(0.1 * data.shape[0])
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+
+MAX_SEQUENCE_LENGTH=seqlen
+EMBEDDING_DIM=20
+from keras.layers import Embedding
+embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            input_length=MAX_SEQUENCE_LENGTH)
+
+
+
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Embedding ,LSTM,TimeDistributed
+from keras.models import Model
+BATCH_SIZE=32
+TIME_STEPS=3
+INPUT_SIZE=1
+model=Sequential()
+'''
+model.add(TimeDistributed(Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            input_length=MAX_SEQUENCE_LENGTH),batch_input_shape=(32,3,5)))
+'''
+model.add(Embedding(len(word_index) + 1, 128))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+'''
+model.add(LSTM(
+    64,       # Or: input_dim=INPUT_SIZE, input_length=TIME_STEPS,
+    
+    return_sequences=True,      # True: output at all steps. False: output as last step.
+    #stateful=True,              # True: the final state of batch1 is feed into the initial state of batch2
+))
+'''
+#x = MaxPooling1D(35)(x)  # global max pooling
+
+model.add(Dense(128, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+              
+              
+model.fit(x_train, y_train, validation_data=(x_val, y_val),
+          epochs=3, batch_size=64)
+          
+          
+model.save('model')
+
+model=load_model('model')
+with open('testing_data.txt','r',encoding='utf-8') as t:
+    testlist=t.readlines()
+testx=[]
+for i in range(1,len(testlist)):
+    testseq=testlist[i].split(',')[1]
+    testx.append(testseq.strip('\n'))
+print(testx[0])
+pre_seq=tokenizer.texts_to_sequences(testx)
+pre_data = pad_sequences(pre_seq, maxlen=seqlen)
+out=model.predict(pre_data)
+pre_l=out.argmax(axis=1).reshape(-1,1)
+print('shape of pre:',pre_l.shape)
+pre_id=np.array(range(0,len(pre_l))).reshape(-1,1)
+pre_l=np.concatenate((pre_id,pre_l),axis=1)
+np.savetxt('predict.csv',pre_l,fmt='%d',delimiter=',',header='id,label',comments='')
 
