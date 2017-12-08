16dcfa976fb3c49c248a83012841d520464cfd17
a45s67 <32732820+a45s67@users.noreply.github.com>
2017-12-08 23:58:53 +0800

    Update hw4_train.sh

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
+wget -O w2vmodel.syn1neg.npy https://www.dropbox.com/s/wpdm4e2n1duelgw/w2vmodel.syn1neg.npy?dl=0
+wget -O w2vmodel.wv.syn0.npy https://www.dropbox.com/s/qe2t64g10fhuux9/w2vmodel.wv.syn0.npy?dl=0
 python hw4train.py $1 $2
