399ba3c604d6580df72c96d84353e8961f27faef
a45s67 <32732820+a45s67@users.noreply.github.com>
2018-01-12 22:59:50 +0800

    Add files via upload

--- hw6/p1_upload.py
+++ hw6/p1_upload.py
@@ -5,7 +5,7 @@ tag_path=sys.argv[2]
 print('img=',img_path)
 print('targ=',tag_path)
 import os
-
+from skimage import io
 
 
 fpath=img_path
@@ -24,7 +24,7 @@ def meanpic(pics):
 
 
 image=[]
-from skimage import io
+
 for jpg in os.listdir(fpath):
     ima=io.imread(os.path.join(fpath,jpg))
     image.append(ima)
@@ -49,6 +49,7 @@ print('V=',V.shape)
 
 
 
+
 targetimg=io.imread(tag_path)
 
 recon=np.zeros((600,600,3))
@@ -60,7 +61,7 @@ for i in range(4):
     io.imsave('eig%d.png'%(i),Uout.reshape(600,600,3))
     '''
     
-    lamd=np.dot(targetimg[j].reshape(-1),U[:,i])
+    lamd=np.dot(targetimg.reshape(-1),U[:,i])
     print('lamd=',lamd)
     recon+=lamd*U[:,i].reshape(600,600,3)
     
